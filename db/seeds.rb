470afb0cf10416e329d498f19f7ce6f51b965787
benja27 <50673336+benja27@users.noreply.github.com>
2023-09-16 19:02:22 -0600

    before capybara

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,39 +11,51 @@
 # Crear usuarios de ejemplo
 # Crear recetas de ejemplo para los usuarios
 
+# require 'faker'
 
 
 
-7.times do |i|
-  Recipe.create!(
-    name: "Receta #{i}",
-    preparation_time: 30.0,
-    cooking_time: 45.0,
-    description: 'Una deliciosa receta de prueba.',
-    public: true,
-    user_id: 1
-  )
+
+# 7.times do |i|
+#   Recipe.create!(
+#     name: "Receta #{i}",
+#     preparation_time: 30.0,
+#     cooking_time: 45.0,
+#     description: 'Una deliciosa receta de prueba.',
+#     public: true,
+#     user_id: 1
+#   )
+# end
+
+
+
+# 14.times do |i|
+#   Food.create!(
+#     name: "Ingrediente #{i}",
+#     measurement_unit: 'grams',
+#     price: 2.5,
+#     quantity: 100,
+#     user: User.first
+#   )
+# end
+
+
+# 35.times do |i|
+#   RecipeFood.create!(
+#     food_id: Food.pluck(:id).sample,  # Asigna un food_id aleatorio de la tabla foods
+#     recipe_id: Recipe.pluck(:id).sample,  # Asigna un recipe_id aleatorio de la tabla recipes
+#     quantity: rand(1.0..10.0)  # Cantidad aleatoria entre 1.0 y 10.0
+#   )
+# end
+require 'factory_bot'
+
+# ... el resto de tus código de semilla ...
+
+
+15.times do |i|
+  FactoryBot.create(:food)
 end
 
-
-
-14.times do |i|
-  Food.create!(
-    name: "Ingrediente #{i}",
-    measurement_unit: 'grams',
-    price: 2.5,
-    quantity: 100,
-    user: User.first
-  )
+15.times do |i|
+  FactoryBot.create(:recipe)
 end
-
-
-35.times do |i|
-  RecipeFood.create!(
-    food_id: Food.pluck(:id).sample,  # Asigna un food_id aleatorio de la tabla foods
-    recipe_id: Recipe.pluck(:id).sample,  # Asigna un recipe_id aleatorio de la tabla recipes
-    quantity: rand(1.0..10.0)  # Cantidad aleatoria entre 1.0 y 10.0
-  )
-end
-
-
