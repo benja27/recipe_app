2ff20053049fd3f2ee898aa1c8baa0e707659651
emarkees <rphillipse@gmail.com>
2023-09-16 22:42:59 -0600

    Update read me file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # Guest user    
-      can :read, Recipe # All users can read recipes
-      can :create, Recipe if user.persisted? # Logged-in users can create recipes
-      can :update, Recipe, user_id: user.id # Users can update their own recipes
-      can :destroy, Recipe, user_id: user.id # Users can delete their own recipes    
+    user ||= User.new # Guest user
+    can :read, Recipe # All users can read recipes
+    can :create, Recipe if user.persisted? # Logged-in users can create recipes
+    can :update, Recipe, user_id: user.id # Users can update their own recipes
+    can :destroy, Recipe, user_id: user.id # Users can delete their own recipes
   end
 end
